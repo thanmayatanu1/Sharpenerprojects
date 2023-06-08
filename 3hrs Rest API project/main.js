3c92ddedce87f082e42c9b533fe3720750b2e02b
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-06-08 11:57:04 +0530

    3HRS rest API javascript project

---
+++ 3hrs Rest API project/main.js
@@ -0,0 +1,92 @@
+function addToNewExpense(event)
+{
+    event.preventDefault();
+    const sellingprice=event.target.sellingprice.value;
+    const productname=event.target.productname.value;
+    const category=event.target.category.value;
+
+    localStorage.setItem("sellingprice", sellingprice);
+    localStorage.setItem("productname", productname);
+    localStorage.setItem("category", category);
+
+    const obj=
+    {
+        "sellingprice":sellingprice,
+        "productname":productname,
+        "category":category,
+    }
+
+    axios.post("https://crudcrud.com/api/832cb351c44b4837ac6dcefec8d1e8b8/appointmentData", obj)
+    .then((response) =>
+    {
+        showUserOnScreen(response.data);
+        console.log(response)
+    })
+    .catch((err) =>
+    {
+        document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>"
+        console.log(err);
+    })
+    
+    // localStorage.setItem(obj.productname, JSON.stringify(obj));
+    // showUserOnScreen(obj);
+
+}
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/832cb351c44b4837ac6dcefec8d1e8b8/appointmentData")
+    .then((response) => {
+        console.log(response)
+        
+        for(var i=0;i<response.data.length;i++){
+            showUserOnScreen(response.data[i])
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+function showUserOnScreen(obj)
+{
+    const parentElement = document.getElementById("list-of-items");
+    const childElement = document.createElement("li");
+    childElement.textContent= obj.sellingprice + " " + obj.productname + " " + obj.category;
+
+    const deleteButton= document.createElement("input");
+    deleteButton.type="button";
+    deleteButton.value="Delete";
+
+    deleteButton.onclick = () =>
+    {
+        // localStorage.removeItem(obj.productname);
+        // parentElement.removeChild(childElement);
+
+        const url = `https://crudcrud.com/api/832cb351c44b4837ac6dcefec8d1e8b8/appointmentData/${obj._id}`;
+        axios.delete(url)
+            .then(() => {
+                parentElement.removeChild(childElement);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+        }
+
+
+    const editButton=document.createElement("input");
+    editButton.type="button";
+    editButton.value="Edit";
+
+    editButton.onclick = () =>
+    {
+        localStorage.removeItem(obj.productname);
+        parentElement.removeChild(childElement);
+        document.getElementById("sellingpriceInputTag").value = obj.sellingprice;
+        document.getElementById("productnameInputTag").value = obj.productname;
+        document.getElementById("categoryInputTag").value=obj.category;
+        
+    }
+
+    childElement.appendChild(deleteButton);
+    childElement.appendChild(editButton);
+    parentElement.appendChild(childElement);
+}
